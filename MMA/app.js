3928473b64e70cb99d490d87697a1e3ca2b3890c
cocopestarino <pestarino35@gmail.com>
2021-12-15 21:46:10 -0300

    correcciones

--- MMA/app.js
+++ MMA/app.js
@@ -9,7 +9,7 @@ app.get('/login', (req,res)=>{
     res.sendFile(path.resolve (__dirname,'views/login.html'))})
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve (__dirname,'views/index.html'))})
+    res.sendFile(path.resolve (__dirname,'views/home.html'))})
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve (__dirname, 'views/register.html'))
